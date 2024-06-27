541a00249aba2789459d2244a0d4a2b4c3e5a738
Wisdom Akpasa <wisdomnemesis@gmail.com>
2024-06-27 09:39:44 +0100

    Adjustments to Start.sln

--- WisdomAkpasa_BU-22C-IT-7153/NumbersDates/DateTimeClass/obj/Debug/net6.0/DateTimeClass.AssemblyInfo.cs
+++ WisdomAkpasa_BU-22C-IT-7153/NumbersDates/DateTimeClass/obj/Debug/net6.0/DateTimeClass.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DateTimeClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d294caa7b39215a1b6363ac27233c0e4164f13c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DateTimeClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DateTimeClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
